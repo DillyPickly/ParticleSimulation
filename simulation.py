c916aaed3972ba2589ec497453054769ea8daf53
DillyPickly <dylan.sabuda@gmail.com>
2019-12-19 19:13:44 -0500

    finished association

--- simulation.py
+++ simulation.py
@@ -13,58 +13,6 @@ Each molecule can then be described as a complex with only alpha sites or only b
 Finally, an alpha site contains an alpha particles and can bond to a beta molecule at that site.
 '''
 
-_x, _y, _z = (2, 2, 2)
-number_cubes = _x * _y * _z
-
-_in_assoc = 0.6  # inherent association probability
-_in_dissoc = 0.1  # inherent dissociation probability
-_valency = 3  # available sites on each molecule (a.k.a. free domains)
-_num_molecules = 3
-_species = 2
-
-# each molecule is described by an index and value gives it the free bonds
-molecules = _valency * np.ones([_num_molecules, _species], dtype=object)
-
-# each complex is described by an id from a set of id's
-free = set([i for i in range(_num_molecules * _species)])
-used = set()
-
-
-complexes = {}
-for i in range(_species):
-    for j in range(_num_molecules):
-        id = free.pop()
-        complexes[id] = np.array([i, j])
-        used.add(id)
-
-# this contains lists of the complexes in each container
-cubes = np.empty([_x, _y, _z], dtype=object)
-for i in range(_x):
-    for j in range(_y):
-        for k in range(_z):
-            cubes[i, j, k] = []
-
-
-def extract_complex(x, y, z):
-    return cubes[x, y, z]
-
-
-def extract_molecules(x, y, z):
-    nearby_complexes = extract_complex(x, y, z)
-    nearby_molecules = []
-    for c in nearby_complexes:
-        for m in complexes[c]:
-            nearby_molecules.append(m)
-
-    return nearby_molecules
-
-
-# def extract_sites(x, y, z):
-#     nearby_molecules = extract_molecules(x, y, z)
-#     nearby sites =
-#     for m in nearby_molecules:
-#         molecules[m[0], m[1]]
-#
 
 def prob_association(alpha, beta):
     return _in_assoc * (1 - _in_dissoc) * 20 / (20 + alpha + beta)
@@ -78,6 +26,63 @@ def prob_dissociation(alpha, beta):
     return _in_dissoc * (1 - _in_assoc)
 
 
+_x, _y, _z = (2, 2, 2)
+number_cubes = _x * _y * _z
+
+_in_assoc = 0.6  # inherent association probability
+_in_dissoc = 0.1  # inherent dissociation probability
+_valency = 3  # available sites on each molecule (a.k.a. free domains)
+_num_molecules = 3
+_species = 2
+
+# each molecule is described by an index and value gives it the free bonds
+molecules = _valency * np.ones([_species, _num_molecules], dtype=object)
+
+# each complex is described by an id from a set of id's
+free = set([i for i in range(_num_molecules * _species)])
+used = set()
+
+complexes = {}
+for i in range(_species):
+    for j in range(_num_molecules):
+        id = free.pop()
+        complexes[id] = [[i, j]]
+        used.add(id)
+
+# print(complexes)
+
+# this contains lists of the complexes in each container
+cubes = np.empty([_x, _y, _z], dtype=object)
+for i in range(_x):
+    for j in range(_y):
+        for k in range(_z):
+            cubes[i, j, k] = []
+
+
+def extract_complex(x, y, z):
+    return cubes[x, y, z]
+
+
+def extract_molecules(x, y, z, complexes):
+    nearby_complexes = extract_complex(x, y, z)
+
+    if len(nearby_complexes) == 0:
+        return None, None
+
+    nearby_molecules = np.empty(_species, dtype=object)
+    for i in range(_species):
+        nearby_molecules[i] = []
+
+    molecule_complex_relations = np.empty_like(molecules)
+    for c in nearby_complexes:
+        for m in complexes[c]:
+            nearby_molecules[m[0]].append(m[1])
+            print('complex key:', c)
+            molecule_complex_relations[m[0], m[1]] = c
+
+    return nearby_molecules, molecule_complex_relations
+
+
 # Initially, randomly distribute alpha and beta complexes
 for i in complexes.keys():
     x = np.random.choice(range(_x))
@@ -88,6 +93,86 @@ for i in complexes.keys():
 # print('cubes: ',cubes)
 
 # go through association then diffusion then dissociation
+for i in range(_x):
+    for j in range(_y):
+        for k in range(_z):
+            nearby_molecules, nearby_complexes = extract_molecules(i, j, k, complexes)
+            if nearby_molecules is None:
+                # print(i,j,k)
+                continue
+            elif len(nearby_molecules[0]) == 0:
+                continue
+            elif len(nearby_molecules[1]) == 0:
+                continue
 
+            # print(nearby_molecules[0])
+            # print(nearby_molecules[1])
 
+            total_sites = np.zeros(_species, dtype=int)
+            # for kind in range(_species):
+            #     total_sites[kind] = np.sum(molecules[kind, nearby_molecules[kind]])
+            # print(total_sites)
+
+            sites = np.zeros(_species, dtype=list)
+            sites[0] = []
+            sites[1] = []
+            for kind in range(_species):
+                for index, s in enumerate(molecules[kind, nearby_molecules[kind]]):
+                    # print(s*[[kind, nearby_molecules[kind][index]]])
+                    total_sites[kind] += s
+                    for _ in range(s):
+                        sites[kind].append([kind, nearby_molecules[kind][index]])
+                    # sites[kind].append(s*[])
+
+            print(total_sites)
+            print(sites)
+
+            if total_sites[0] < total_sites[1]:
+                smaller = 0
+                larger = 1
+            else:
+                smaller = 1
+                larger = 0
+
+            prob = prob_association(total_sites[0], total_sites[1])
+            p = [prob, 1 - prob]
+            assoc = np.random.choice(2, total_sites, p=p)
+            # print(assoc)
+
+            new_bonds = []
+            for m in range(total_sites[0]):
+                for n in range(total_sites[1]):
+                    if assoc[m, n] == 1:
+                        assoc[:, n] = 0
+                        assoc[m, :] = 0
+                        assoc[m, n] = 1
+                        new_bonds.append([m, n])
+                        break
+
+            for n in new_bonds:
+                print(n)
+                print(sites[0][n[0]])
+                print(sites[1][n[1]])
+
+                molecules[sites[0][n[0]][0], sites[0][n[0]][1]] -= 1
+                molecules[sites[1][n[1]][0], sites[1][n[1]][1]] -= 1
+
+                complex_key_a = nearby_complexes[sites[0][n[0]][0], sites[0][n[0]][1]]
+                complex_key_b = nearby_complexes[sites[1][n[1]][0], sites[1][n[1]][1]]
+                print('nearby_complexes', nearby_complexes)
+
+                if complex_key_a != complex_key_b:
+                    print(complexes.keys())
+                    print('complex_key_b', complexes[complex_key_b])
+                    print('complex_key_a', complexes[complex_key_a])
+
+                    complexes[complex_key_a] += complexes[complex_key_b]
+                    used.remove(complex_key_b)
+                    free.add(complex_key_b)
+                    del complexes[complex_key_b]
+
+                    nearby_complexes[sites[1][n[1]][0], sites[1][n[1]][1]] = complex_key_a
+                    # print(complexes[complex_key_a])
+
+            print(assoc)
 # association of complexes (in each cube)
